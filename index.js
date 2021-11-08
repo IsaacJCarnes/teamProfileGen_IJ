18f493862f0be37c7bfee5029d5bd62b8ff7930c
Isaac Joshua <isaacjcarnes@gmail.com>
2021-11-08 18:59:17 -0500

    Added exports to class files

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-const { jest } = require("@jest/globals");
+const jest = require("jest")
 const inquirer = require("inquirer");
+const Employee = require("./lib/Employee");
 const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+let int = new Intern("James", 1, "fart@gmail.com","Harvard");
+let mana = new Manager("Jerk", 2, "Cancel@gmail", 45);
+
+console.log(int);
+console.log(mana);
