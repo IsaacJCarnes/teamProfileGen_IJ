c1d9e6409221df79cab622d5b121ebcc1cf1308f
Isaac Joshua <isaacjcarnes@gmail.com>
2021-11-08 20:02:41 -0500

    Created basic input loop

--- index.js
+++ index.js
@@ -5,8 +5,67 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 
-let int = new Intern("James", 1, "fart@gmail.com","Harvard");
-let mana = new Manager("Jerk", 2, "Cancel@gmail", 45);
+let manager = null;
+const teamMembers = [];
 
-console.log(int);
-console.log(mana);
+function addEngineer(){
+
+}
+
+function addIntern(){
+
+}
+
+function addEmployee(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'Would you like to add a member to the team?',
+            name: 'memberType',
+            choices: [memberOptions[0], memberOptions[1], memberOptions[2]]
+        }
+    ]).then((response) =>{
+        switch (response.memberType){
+            case  memberOptions[0]: // Engineer
+                addEngineer();
+                return true;
+            case memberOptions[1]: // Intern
+                addIntern();
+                return true;
+            case memberOptions[2]: // No more members
+                return false;          
+        }
+    });
+}
+
+inquirer
+    .prompt([
+    {
+        type: 'input',
+        message: 'What is the team managers name?',
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: 'What is the team managers employee ID?',
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: 'What is the team managers email?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'What is the team managers office number?',
+        name: 'officeNum',
+    }
+
+]).then((response) => {
+    manager = new Manager(response.name, response.id, response.email, response.officeNum);
+    let addingMembers = true;
+    let memberOptions = ["Add an engineer", "Add an intern","Done adding team members"];
+    while(addingMembers){
+        addingMembers = addEmployee();
+    }
+});
